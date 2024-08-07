d82e4489ad4948f4cd09ff7652eecc2a6a389669
hariV0078 <145679215+hariV0078@users.noreply.github.com>
2024-08-07 12:03:49 +0530

    Create Day1CE3.js

---
+++ Day1/Day1CE3.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import './App.css';
+ const World=()=>{
+  return(
+    <div>
+      <p>This is World Functional Component</p>
+    </div>
+  )
+ }
+
+
+export default World;
