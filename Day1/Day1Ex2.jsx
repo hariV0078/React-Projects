4238960326f1618efd3e0e4c9e21da94fd12e91d
hariV0078 <145679215+hariV0078@users.noreply.github.com>
2024-08-07 12:03:02 +0530

    Update Day1CEx2.jsx

--- Day1/Day1Ex2.jsx
+++ Day1/Day1Ex2.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React from 'react';
+function qn2(){
+    let myobject={name:"Madras"};
+    let newObject=myobject;
+    myobject.name="Chennai";
+    console.log(newObject);
+  
+    let myarray=["a","e","i","o","u"];
+    let vowelArray=myarray;
+    myarray.push("u");
+    console.log(vowelArray);
+    alert("Check the console output!");
+  }
+  
+const App3 = () => 
+{
+  return(
+    <div>
+        <button onClick={qn2}>click</button>
+    </div>
+  )
 
-const Greeting = () => {
-    return(
-        <div>
-            <h2>Good Morning</h2>
-        </div>
-    )
 }
-export default Greeting;
+export default App3;
