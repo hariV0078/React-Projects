b1256f5f9b6c1b90404200f1c5ec578c025b7de5
hariV0078 <145679215+hariV0078@users.noreply.github.com>
2024-09-02 13:58:03 +0530

    Add files via upload

---
+++ Day3/day3_PH1.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function TransformationToggle() {
+  
+  const [transformation, setTransformation] = useState('Kaioken');
+
+ 
+  const toggleTransformation = () => {
+    setTransformation((prevTransformation) =>
+      prevTransformation === 'Kaioken' ? 'SuperSaiyan' : 'Kaioken'
+    );
+  };
+
+  return (
+    <div>
+      
+      <h2>Current Transformation: {transformation}</h2>
+      
+      
+      <button onClick={toggleTransformation}>Toggle Transformation</button>
+    </div>
+  );
+}
+
+export default TransformationToggle;
