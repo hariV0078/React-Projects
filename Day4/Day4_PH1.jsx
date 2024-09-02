b57d7510728f9c13760f597902aa44fddc6c1138
hariV0078 <145679215+hariV0078@users.noreply.github.com>
2024-09-02 15:12:00 +0530

    Update Day4_PH1.jsx

--- Day4/Day4_PH1.jsx
+++ Day4/Day4_PH1.jsx
@@ -1,1 +1,27 @@
 
+import React, { useState } from 'react';
+
+function TransformationToggle() {
+  
+  const [transformation, setTransformation] = useState('Kaioken');
+
+ 
+  const toggleTransformation = () => {
+    setTransformation((prevTransformation) =>
+      prevTransformation === 'Kaioken' ? 'SuperSaiyan' : 'Kaioken'
+    );
+  };
+
+  return (
+    <div>
+      
+      <h2>Current Transformation: {transformation}</h2>
+      
+      
+      <button onClick={toggleTransformation}>Toggle Transformation</button>
+    </div>
+  );
+}
+
+export default TransformationToggle;
+
